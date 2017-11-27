2da100ac079c8b1c7ca3ac9a432e14d278374e1a
Tarek224 <33825981+Tarek224@users.noreply.github.com>
2017-11-27 12:27:04 +0200

    Add files via upload

--- prp6.asm
+++ prp6.asm
@@ -1,82 +1,68 @@
-﻿.586
+.586
 .MODEL FLAT
 INCLUDE io.h
 .STACK 4096
 .DATA
-   prompt BYTE "ENTER a grade", 0
-   promp BYTE "ENTER a weight", 0
-   g1 DWORD ?
-   g2 DWORD ?
-   g3 DWORD ?
-   g4 DWORD ?
-   w1 DWORD ?
-   w2 DWORD ?
-   w3 DWORD ?
-   w4 DWORD ?
-   
-   wofsum BYTE 40 DUP(?),0
-   sumofw BYTE 40 DUP(?),0
-   avg BYTE 40 DUP(?),0
-   Result BYTE 40 DUP(?)
-   resultbl BYTE "the weighted sum is", 0
-   resultb BYTE "the sum of weight is", 0
-   resu BYTE "the weighted average", 0
+	pr_1	BYTE "Enter the Grade", 0
+	pr_2	BYTE "Enter the Weight", 0
+	res_1	BYTE "The Weighted Sum", 0
+	res_2	BYTE "The Sum of Weights", 0
+	res_3	BYTE "The Average", 0
+	inp		BYTE 40 DUP (?)
+	x		DWORD ?
+	y		DWORD ?
+	z		DWORD ?
 .CODE
-MainProc PROC
-   input prompt,Result,40
-   atod Result
-   mov g1,eax
-   input promp,Result,40
-   atod Result
-   mov w1,eax
-   input prompt,Result,40
-   atod Result
-   mov g2,eax
-   input promp,Result,40
-   atod Result
-   mov w2,eax
-    input prompt,Result,40
-   atod Result
-   mov g3,eax
-   input promp,Result,40
-   atod Result
-   mov w3,eax
-   input prompt,Result,40
-   atod Result
-   mov g4,eax
-   input promp,Result,40
-   atod Result
-   mov w4,eax
-
-   mov eax,g1
-   imul eax,w1
-   mov g1,eax
-   mov eax,g2
-   imul eax,w2
-   mov g2,eax
-   mov eax,g3
-   imul eax,w3
-   mov g3,eax
-   mov eax,g4
-   imul eax,w4
-   add eax,g1
-   add eax,g2
-   add eax,g3
-   mov ebx,eax
-   dtoa wofsum,eax
-   output resultbl,wofsum
-   mov eax,w1
-   add eax,w2
-   add eax,w3
-   add eax,w4
-   mov ecx,eax
-   dtoa sumofw,eax
-   output resultb,sumofw
-   mov eax,ebx
-   div ecx
-   dtoa avg,eax
-   output resu,avg
-   mov eax, 0
-   ret
-MainProc ENDP
+	_MainProc PROC
+		input	pr_1, inp, 40
+		atod	inp
+		mov		x, eax
+		input	pr_2, inp, 40
+		atod	inp
+		mov		y, eax
+		imul	eax, x
+		mov		z, eax
+		input	pr_1, inp, 40
+		atod	inp
+		mov		x, eax
+		input	pr_2, inp, 40
+		atod	inp
+		mov		ebx, eax
+		add		y, ebx
+		imul	eax, x
+		add		z, eax
+		input	pr_1, inp, 40
+		atod	inp
+		mov		x, eax
+		input	pr_2, inp, 40
+		atod	inp
+		mov		ebx, eax
+		add		y, ebx
+		imul	eax, x
+		add		z, eax
+		input	pr_1, inp, 40
+		atod	inp
+		mov		x, eax
+		input	pr_2, inp, 40
+		atod	inp
+		mov		ebx, eax
+		add		y, ebx
+		imul	eax, x
+		add		z, eax
+		mov		eax, z
+		mov		x, eax
+		dtoa	z, eax
+		output	res_1,z
+		mov		eax, y
+		dtoa	z, eax
+		output	res_2, z
+		mov		ebx, y
+		mov		edx, 0
+		mov		eax, x
+		div		ebx
+		dtoa	z, eax
+		output	res_3, z
+		mov    eax, 0
+		ret
+	_MainProc ENDP
 END
